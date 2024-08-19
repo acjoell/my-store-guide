f5bf558baf3ce9a27db8cf715956e40e5e526f6e
Joel <joelac04@hotmail.com>
2024-08-19 18:37:55 -0500

    access with CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routerAPI = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const cors = require('cors')
 
 // creamos APP
 const APP = express()
@@ -8,6 +9,19 @@ const PORT = process.env.PORT
 
 // middleware: recibimos datos
 APP.use(express.json())
+
+// Acceso a nuestra API
+const whitelist = ['http://localhost:8080', 'https://joel_app.co', 'http://127.0.0.1:5500']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Acceso no garantizado'))
+    }
+  }
+}
+APP.use(cors(options))
 
 APP.get('/', (req, res) => {
   res.send('Hola mundo')
