bafa1a1168f31f809421be90403bdacfca719e6b
Joel <joelac04@hotmail.com>
2024-09-23 11:48:26 -0500

    [feat] running first settings for migrations

--- api/config/config.js
+++ api/config/config.js
@@ -1,10 +1,10 @@
 const config = {
   env: process.env.NODE_ENV || 'dev',
-  dbUser: process.env.PG_USER,
-  dbPassword: process.env.PG_PASSWORD,
-  dbHost: process.env.PG_HOST,
-  dbPort: process.env.PG_PORT,
-  dbName: process.env.PG_NAME,
+  dbUser: process.env.PG_USER || 'acjoell',
+  dbPassword: process.env.PG_PASSWORD || 'admin123',
+  dbHost: process.env.PG_HOST || '172.18.0.2',
+  dbPort: process.env.PG_PORT || 5432,
+  dbName: process.env.PG_NAME || 'my_store',
 }
 
 module.exports = { config }
--- api/db/config.js
+++ api/db/config.js
@@ -1,23 +1,23 @@
-const { config } = require('./../config/config')
+const {config} = require('./../config/config.js')
+  
+  const USER = encodeURIComponent(config.dbUser)
+  const PSWD = encodeURIComponent(config.dbPassword)
+  const URI = `postgres://${USER}:${PSWD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-const USER = encodeURIComponent(config.dbUser)
-const PSWD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PSWD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
-
-module.exports = {
-  development: {
-    url: URI,
-    dialect: 'postgres',
-    "database": "database_development",
-  },
-  "test": {
-    url: URI,
-    dialect: 'postgres',
-    //database: 'database_test',
-  },
-  "production": {
-    url: URI,
-    dialect: 'postgres',
-    //database: 'database_production',
+  module.exports = {
+    development: {
+      url: URI,
+      dialect: 'postgres',
+      //"database": "database_development",
+    },
+    "test": {
+      url: URI,
+      dialect: 'postgres',
+      //database: 'database_test',
+    },
+    "production": {
+      url: URI,
+      dialect: 'postgres',
+      //database: 'database_production',
+    }
   }
-}
